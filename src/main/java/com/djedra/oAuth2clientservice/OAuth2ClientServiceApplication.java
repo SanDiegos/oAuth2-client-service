baaa2c92db39c8df70399270d5996e016332d3d5
SanDiegos <52674560+SanDiegos@users.noreply.github.com>
2019-12-26 17:20:07 +0100

    Add files via upload

---
+++ src/main/java/com/djedra/oAuth2clientservice/OAuth2ClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.djedra.oAuth2clientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2ClientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djedra/oAuth2clientservice/OAuth2ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djedra.oAuth2clientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
